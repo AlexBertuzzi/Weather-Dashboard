fc186a317aeceb4c0c920418d590849afd459ead
AlexBertuzzi <alexmichaelbertuzzi@yahoo.com>
2020-11-21 13:49:05 -0600

    local storage works and buttons are being created

--- script.js
+++ script.js
@@ -1,36 +1,75 @@
 
-$("#searchBtn").on("click", function(){
-    var searchInput = $("#searchInput").val().trim();
-    var APIkey = "c0d3d17620b893f264681d297097a6e0";
-    var currentURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput + "&appid=" + APIkey;
-    var forcastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchInput + "&appid=" + APIkey;
+var searchInput = JSON.parse(localStorage.getItem("searchInput"));
+loadWeather()
 
-    $.ajax({
-        url: currentURL,
-        method: "GET"
-    }).then(function(res1){
-        console.log(res1);
-        $("#city").text(res1.name + " " + res1.weather[0].description);
-        var tempF = (res1.main.temp - 273.15) * 1.80 + 32;
-        $("#temp").text(tempF.toFixed(2) + " Fahrenheit");
-        $("#hum").text("Humidity: " + res1.main.humidity + "%");
-        $("#wind").text("Wind Speed: " + res1.wind.speed + " mph");
-        var lat = res1.coord.lat;
-        var lon = res1.coord.lon;
-        var uvURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=" + APIkey;
-        $.ajax({
-            url: uvURL,
-            method: "GET"
-        }).then(function(resUV){
-            console.log(resUV);
-            $("#uv").text("UV Index: " + resUV.value)
-        })
-    });
+$("#searchBtn").on("click", function(event){
+    event.preventDefault();
+    searchInput = $("#searchInput").val().trim();
+    loadWeather();
+    var recent = $("<p>")
+    recent.attr("id", "newBtn")
+    recent = recent.text(JSON.parse(localStorage.getItem("searchInput")));
+    $("#recent").prepend(recent);
+});  
 
-    $.ajax({
-        url: forcastURL,
-        method: "GET"
-    }).then(function(res2){
-        console.log(res2);
-    })
+$("#newBtn").on("click", function(){
+    //event.preventDefault();
+    searchInput = $("#newBtn").text();
+    console.log(searchInput)
+    loadWeather();
 });
+    function loadWeather(){
+        var APIkey = "c0d3d17620b893f264681d297097a6e0";
+        var currentURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput + "&appid=" + APIkey;
+        
+        $.ajax({
+            url: currentURL,
+            method: "GET"
+        }).then(function(res1){
+            console.log(res1);
+            localStorage.setItem("searchInput", JSON.stringify(res1.name));
+            var date = moment.unix(res1.dt).format("L");
+            $("#city").text(res1.name + " " + date);
+            $("#icon").attr("src", "http://openweathermap.org/img/wn/" + res1.weather[0].icon + ".png")
+            var lat = res1.coord.lat;
+            var lon = res1.coord.lon;
+            var forcastURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&exclude=minutely,hourly,alerts&appid=" + APIkey;
+            
+            $.ajax({
+                url: forcastURL,
+                method: "GET"
+            }).then(function(res2){
+                console.log(res2);
+                $("#hum").text("Humidity: " + res2.current.humidity + "%");
+                $("#temp").text(res2.current.temp + " Fahrenheit");
+                $("#wind").text("Wind Speed: " + res2.current.wind_speed + " mph");
+                $("#uv").text("UV Index: " + res2.current.uvi);
+
+                $("#d1").text(moment.unix(res2.daily[1].dt).format("L"));
+                $("#i1").attr("src", "http://openweathermap.org/img/wn/" + res2.daily[1].weather[0].icon + ".png")
+                $("#t1").text(res2.daily[1].temp.day + " Fahrenheit")
+                $("#h1").text("Humidity: " + res2.daily[1].humidity + "%")
+
+                $("#d2").text(moment.unix(res2.daily[2].dt).format("L"));
+                $("#i2").attr("src", "http://openweathermap.org/img/wn/" + res2.daily[2].weather[0].icon + ".png")
+                $("#t2").text(res2.daily[2].temp.day + " Fahrenheit")
+                $("#h2").text("Humidity: " + res2.daily[2].humidity + "%")
+
+                $("#d3").text(moment.unix(res2.daily[3].dt).format("L"));
+                $("#i3").attr("src", "http://openweathermap.org/img/wn/" + res2.daily[3].weather[0].icon + ".png")
+                $("#t3").text(res2.daily[3].temp.day + " Fahrenheit")
+                $("#h3").text("Humidity: " + res2.daily[3].humidity + "%")
+
+                $("#d4").text(moment.unix(res2.daily[4].dt).format("L"));
+                $("#i4").attr("src", "http://openweathermap.org/img/wn/" + res2.daily[4].weather[0].icon + ".png")
+                $("#t4").text(res2.daily[4].temp.day + " Fahrenheit")
+                $("#h4").text("Humidity: " + res2.daily[4].humidity + "%")
+
+                $("#d5").text(moment.unix(res2.daily[5].dt).format("L"));
+                $("#i5").attr("src", "http://openweathermap.org/img/wn/" + res2.daily[5].weather[0].icon + ".png")
+                $("#t5").text(res2.daily[5].temp.day + " Fahrenheit")
+                $("#h5").text("Humidity: " + res2.daily[5].humidity + "%")
+            })
+        });
+    }
+
